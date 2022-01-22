bd08d9fac2f89bcf804be999a99117986ef23255
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2022-01-22 12:09:06 -0300

    up

--- lib/pages/places_form_page.dart
+++ lib/pages/places_form_page.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:localtion_app/widgets/app_bar_widget.dart';
 
-class PlacesFormPage extends StatelessWidget {
+class PlacesFormPage extends StatefulWidget {
   const PlacesFormPage({Key? key}) : super(key: key);
 
+  @override
+  State<PlacesFormPage> createState() => _PlacesFormPageState();
+}
+
+class _PlacesFormPageState extends State<PlacesFormPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(
-        title: 'Adicionar Lugares',
+        title: 'Adicionar Novo Lugar',
         centerTitle: true,
       ),
       body: _body(context),
